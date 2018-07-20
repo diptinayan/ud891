fa1f389a611a959571bb41da01bcecf7ddad00d8
Dipti Nayan <diptinayan21@gmail.com>
2018-07-20 12:13:40 +0530

    Update radiogroup.js

--- lesson5-semantics-aria/06-radio-group/radiogroup.js
+++ lesson5-semantics-aria/06-radio-group/radiogroup.js
@@ -24,6 +24,8 @@
     this.el.addEventListener('click', this.handleClick.bind(this));
 
     // Any more initialization to do here?
+    
+    this.el.setAttribute('role','radiogroup');
 
     var firstButton = true;
     for (var button of this.buttons) {
@@ -35,6 +37,8 @@
       }
 
       // What about here?
+      
+      button.setAttribute('role','radio');
     }
 
   }
@@ -98,6 +102,7 @@
     this.focusedButton.tabIndex = 0;
     this.focusedButton.focus();
     this.focusedButton.setAttribute('checked', '');
+    this.focusedButton.setAttribute('aria-checked','true');
 
     // ... we probably want to do some stuff here, too ...
 
