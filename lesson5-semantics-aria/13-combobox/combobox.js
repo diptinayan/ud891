f19b051ce8596d7ca52a87f80d98f935ca240650
Dipti Nayan <diptinayan21@gmail.com>
2018-07-23 00:46:29 +0530

    Update combobox.js

--- lesson5-semantics-aria/13-combobox/combobox.js
+++ lesson5-semantics-aria/13-combobox/combobox.js
@@ -170,6 +170,13 @@
                 this.hide();
             } else {
                 // FIXME: ChromeVox reports the wrong list size and position
+                
+                for(var i=0;i<this.visibleItems.length;i++){
+                var item = this.visible.Items[i];
+                    item.setAttribute('aria-posinset',i+1);
+                    item.setAttribute('aria-setsize',this.visiblesItems.length);
+                    
+                }
             }
         },
 
@@ -237,6 +244,7 @@
             newActive.classList.add('active');
 
             // FIXME: need to ensure focus stays on textbox, but report active list option
+            this.textbox.setActiveDescendant(newActive);
         }
     };
 
